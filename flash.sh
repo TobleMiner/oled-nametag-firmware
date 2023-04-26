cd32be0c9897f19a87689248e22074e95afae8c0
Tobias Schramm <tobias@t-sys.eu>
2023-04-26 18:51:03 +0200

    Add hacky workaround for failure to flash large application images

---
+++ flash.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e -o pipefail
+
+idf.py build
+
+dd if=build/oled_nametag.bin of=build/oled_nametag1.bin bs=4096 count=100 &> /dev/null
+dd if=build/oled_nametag.bin of=build/oled_nametag2.bin bs=4096 skip=100 &> /dev/null
+
+esptool.py -p /dev/ttyACM* -b 460800 --before default_reset --after hard_reset --chip esp32s3  write_flash --flash_mode dio --flash_size detect --flash_freq 80m \
+	0x0 build/bootloader/bootloader.bin \
+	0x10000 build/partition_table/partition-table.bin \
+	0x18000 build/ota_data_initial.bin \
+	0x20000 build/oled_nametag1.bin \
+	0x84000 build/oled_nametag2.bin
+
+case $1 in
+	*monitor*) idf.py -p /dev/ttyACM* monitor;;
+esac
