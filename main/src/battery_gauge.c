425111e00737857e9c3c686c5c51e6c6700899b7
Tobias Schramm <tobias@t-sys.eu>
2023-05-18 20:24:39 +0200

    battery gauge: add exponential backoff on readout failure
    Do not hammer the battey gauage with requests every 2s if it is not
    responding.

--- main/src/battery_gauge.c
+++ main/src/battery_gauge.c
@@ -8,13 +8,15 @@
 #include "scheduler.h"
 #include "bq27546.h"
 
-#define GAUGE_I2C_BUS		I2C_NUM_0
-#define UPDATE_INTERVAL_US	MS_TO_US(2000)
+#define GAUGE_I2C_BUS			I2C_NUM_0
+#define UPDATE_INTERVAL_US		MS_TO_US(2000)
+#define UPDATE_INTERVAL_MAX_BACKOFF	4
 
 static const char *TAG = "battery gauge";
 
 static bq27546_t bq_gauge;
 static scheduler_task_t gauge_update_task;
+static int gauge_update_interval_exponent = 0;
 
 static unsigned int battery_voltage_mv = 0;
 static int battery_current_ma = 0;
@@ -22,6 +24,7 @@ static unsigned int battery_soc_percent = 0;
 static unsigned int battery_soh_percent = 0;
 static unsigned int battery_time_to_empty_min = 0;
 static int battery_temperature_0_1degc = 0;
+static bool battery_gauge_healthy = false;
 
 #define POWEROFF_THRESHOLD_MV	2800
 #define POWEROFF_SAMPLES	5
@@ -29,6 +32,23 @@ static int battery_temperature_0_1degc = 0;
 static unsigned int samples_below_poweroff_threshold = 0;
 
 static void battery_gauge_update(void *ctx);
+static void battery_gauge_schedule_update(bool success) {
+	uint32_t update_deadline_us;
+
+	if (success) {
+		gauge_update_interval_exponent = 0;
+		battery_gauge_healthy = true;
+	} else {
+		if (gauge_update_interval_exponent < UPDATE_INTERVAL_MAX_BACKOFF) {
+			gauge_update_interval_exponent++;
+		}
+		battery_gauge_healthy = false;
+	}
+
+	update_deadline_us = UPDATE_INTERVAL_US << gauge_update_interval_exponent;
+	scheduler_schedule_task_relative(&gauge_update_task, battery_gauge_update, NULL, update_deadline_us);
+}
+
 static void battery_gauge_update(void *ctx) {
 	int voltage_mv = bq27546_get_voltage_mv(&bq_gauge);
 	int current_ma;
@@ -37,6 +57,7 @@ static void battery_gauge_update(void *ctx) {
 	int temperature_0_1k = bq27546_get_temperature_0_1k(&bq_gauge);
 	int time_to_empty_min = bq27546_get_time_to_empty_min(&bq_gauge);
 	esp_err_t err;
+	bool parameter_get_success = true;
 
 	if (voltage_mv >= 0) {
 		if (battery_voltage_mv != voltage_mv) {
@@ -53,6 +74,7 @@ static void battery_gauge_update(void *ctx) {
 		battery_voltage_mv = voltage_mv;
 	} else {
 		ESP_LOGE(TAG, "Failed to read battery voltage: %d", voltage_mv);
+		parameter_get_success = false;
 	}
 
 	if (soc_percent >= 0) {
@@ -62,6 +84,7 @@ static void battery_gauge_update(void *ctx) {
 		battery_soc_percent = soc_percent;
 	} else {
 		ESP_LOGE(TAG, "Failed to read state of charge: %d", soc_percent);
+		parameter_get_success = false;
 	}
 
 	if (soh_percent >= 0) {
@@ -71,6 +94,7 @@ static void battery_gauge_update(void *ctx) {
 		battery_soh_percent = soh_percent;
 	} else {
 		ESP_LOGE(TAG, "Failed to read state of health: %d", soh_percent);
+		parameter_get_success = false;
 	}
 
 	if (temperature_0_1k >= 0) {
@@ -82,6 +106,7 @@ static void battery_gauge_update(void *ctx) {
 		battery_temperature_0_1degc = temperature_0_1degc;
 	} else {
 		ESP_LOGE(TAG, "Failed to read state of health: %d", soh_percent);
+		parameter_get_success = false;
 	}
 
 	if (time_to_empty_min >= 0) {
@@ -91,11 +116,13 @@ static void battery_gauge_update(void *ctx) {
 		battery_time_to_empty_min = time_to_empty_min;
 	} else {
 		ESP_LOGE(TAG, "Failed to read state time to empty: %d", time_to_empty_min);
+		parameter_get_success = false;
 	}
 
 	err = bq27546_get_current_ma(&bq_gauge, &current_ma);
 	if (err) {
 		ESP_LOGE(TAG, "Failed to read battery current: %d", err);
+		parameter_get_success = false;
 	} else {
 		if (battery_current_ma != current_ma) {
 			ESP_LOGI(TAG, "Battery current: %d mA", current_ma);
@@ -103,13 +130,21 @@ static void battery_gauge_update(void *ctx) {
 		battery_current_ma = current_ma;
 	}
 
+	battery_gauge_schedule_update(parameter_get_success);
 	event_bus_notify("battery_gauge", NULL);
-	scheduler_schedule_task_relative(&gauge_update_task, battery_gauge_update, NULL, UPDATE_INTERVAL_US);
 }
 
 void battery_gauge_init(void) {
-	ESP_ERROR_CHECK(bq27546_init(&bq_gauge, GAUGE_I2C_BUS));
-	scheduler_schedule_task_relative(&gauge_update_task, battery_gauge_update, NULL, UPDATE_INTERVAL_US);
+	esp_err_t err;
+
+	err = bq27546_init(&bq_gauge, GAUGE_I2C_BUS);
+
+	if (err) {
+		ESP_LOGE(TAG, "Failed to initialize battery gauge: %d", err);
+	} else {
+		battery_gauge_healthy = true;
+		scheduler_schedule_task_relative(&gauge_update_task, battery_gauge_update, NULL, 0);
+	}
 }
 
 void battery_gauge_stop(void) {
@@ -142,4 +177,8 @@ unsigned int battery_gauge_get_time_to_empty_min(void) {
 
 int battery_gauge_get_temperature_0_1degc(void) {
 	return battery_temperature_0_1degc;
+}
+
+bool battery_gauge_is_healthy(void) {
+	return battery_gauge_healthy;
 }
--- main/src/battery_gauge.h
+++ main/src/battery_gauge.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <stdbool.h>
+
 void battery_gauge_init(void);
 void battery_gauge_stop(void);
 void battery_gauge_start(void);
@@ -10,3 +12,4 @@ unsigned int battery_gauge_get_soc_percent(void);
 unsigned int battery_gauge_get_soh_percent(void);
 unsigned int battery_gauge_get_time_to_empty_min(void);
 int battery_gauge_get_temperature_0_1degc(void);
+bool battery_gauge_is_healthy(void);
